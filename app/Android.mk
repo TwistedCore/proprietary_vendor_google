e52088b53fee34254505b976035cd738a06cb8bf
aow1980 <wilkes.allen@gmail.com>
2016-09-25 11:13:05 -0400

    Add prebuilt Google DeskClock, Calendar, Calculator
    Some gapps packages like OpenGapps may remove the prebuilts. Banks Gapps leave them in place so far.
    
    Following in the footsteps of beanstown106 and adding a few apps I think most people use and prefer over the Aosp ones.

---
+++ app/Android.mk
@@ -0,0 +1,46 @@
+# Copyright (C) 2016 The Pure Nexus Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := CalendarGooglePrebuilt
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := CalendarGooglePrebuilt/CalendarGooglePrebuilt.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_OVERRIDES_PACKAGES := Calendar
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := PrebuiltDeskClockGoogle
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := PrebuiltDeskClockGoogle/PrebuiltDeskClockGoogle.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_OVERRIDES_PACKAGES := DeskClock
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := CalculatorGoogle
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := CalculatorGoogle/CalculatorGoogle.apk
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_MODULE_CLASS := APPS
+LOCAL_OVERRIDES_PACKAGES := ExactCalculator
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
